fe79e9b431c811ec1878466014170d175a1ee4f2
unknown <a1swdeveloper@yahoo.com>
2023-04-05 21:37:52 -0400

    Forms started, before Advanced Forms

---
+++ Section6Forms/Models/MilkShakeOrderModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section6Forms.Models
+{
+    public class MilkShakeOrderModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Invalid Name Length.")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Invalid Type Length.")]
+        public string Type { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Invalid Size Length.")]
+        public string Size { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Choose a quantity between 1 and 10.")]
+        public int Quantity { get; set; }
+
+        [Range(typeof(bool), "true", "false", ErrorMessage = "You need to choose whipped cream or not.")]
+        public bool WhippedCream { get; set; }
+    }
+}
---
+++ Section6Forms/Models/PersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section6Forms.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 5, ErrorMessage = "Invalid First Name Length.")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = "Invalid Last Name Length.")]
+        public string LastName { get; set; }
+
+        // Added for Validation
+
+        [Range(0, 125, ErrorMessage = "You Need to enter a valid age.")]
+        public int Age { get; set; }
+
+        // false not allowed
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You need to give consent to continue")]
+        public bool GaveConsent { get; set; }
+    }
+}
